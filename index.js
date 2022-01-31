28ba8158d987dae775897e46525c2a57f6ff0dac
William Warner Burke <williamburke121@gmail.com>
2022-01-31 12:02:30 -0500

    send it

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2 
+const random = Math.ceil(Math.random())
+let num3 = 14
+let num4= 10
+let mod = num3 % num4 
+let max = Math.max(1, 2, 20)
